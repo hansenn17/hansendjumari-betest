6561f50d03927c578494db1235c91332896a3a65
hansendjumari <hansendjumari.mm1@gmail.com>
2024-11-10 00:48:13 +0700

    feat(hansendjumari-betest): add authentication middleware and api

---
+++ app/routes/auth.routes.ts
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import * as authController from "../controller/auth.controller";
+
+const router: Router = express.Router();
+
+router.post("/token", authController.generateToken);
+
+export default router;
--- index.ts
+++ index.ts
@@ -1,11 +1,17 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
-import { connectToDatabase } from "./app/database/connection";
+
+import connectToDatabase from "./app/config/database";
+import authRoutes from "./app/routes/auth.routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+
+app.use("/api/auth", authRoutes);
+
+connectToDatabase();
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
